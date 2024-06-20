0fbd5de912f770948321a43760a3663d1721e00b
Diego Lázaro <111025421+diegolazarocs@users.noreply.github.com>
2024-06-20 00:16:44 -0300

    Add files via upload

---
+++ Custom Select Menu Dropdown/main.js
@@ -0,0 +1,10 @@
+function show(anithing) {
+  document.querySelector(".textBox").value = anithing;
+}
+
+let dropdown = document.querySelector(".dropdown");
+dropdown.onclick = function () {
+  dropdown.onclick = function () {
+    dropdown.classList.toggle("active");
+  };
+};
